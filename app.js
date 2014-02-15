e261107ada455e9efed0363bc81012d8f0fb9740
Stepan Suvorov <stevermeister@gmail.com>
2014-02-15 09:44:19 +0100

    set initial model

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 window.app = angular.module('feedthedev',[]);
 
 app.controller('mainController', function($scope){
-  $scope.test = '!!!';
+  $scope.changelog = [
+    {userId: 37,
+     logId: 11128,
+     description: 'lorem ipsum'
+    },
+    {userId: 49,
+      logId: 13328,
+      description: 'lorem ipsum2'
+    }
+  ];
 });
