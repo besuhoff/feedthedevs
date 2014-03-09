79e994c51cde1b66ad25bfb9d74531d0bb2df0e8
Stepan Suvorov <stevermeister@gmail.com>
2014-03-09 13:50:20 +0100

    update db dump structure

--- dump.sql
+++ dump.sql
@@ -1,13 +1,20 @@
 # Dump of table marks
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `marks`;
-
 CREATE TABLE `marks` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `release_id` int(11) DEFAULT NULL,
   `feed` enum('pizza','tomato') NOT NULL DEFAULT 'pizza',
-  PRIMARY KEY (`id`),
   UNIQUE KEY `one_mark` (`user_id`,`release_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+# Dump of table marks_cache
+# ------------------------------------------------------------
+
+CREATE TABLE `marks_cache` (
+  `release_id` int(11) unsigned NOT NULL,
+  `pizza` int(11) DEFAULT NULL,
+  `tomato` int(11) DEFAULT NULL,
+  PRIMARY KEY (`release_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
