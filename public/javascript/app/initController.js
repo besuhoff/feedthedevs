b27bffdde4f8b4187137caf51d308baa1c5413ac
Serg Pereversev <spereversev@envionsoftware.com>
2014-08-13 19:20:06 +0300

    Tell if the repo is not found; prevent loading senseless page

--- public/javascript/app/initController.js
+++ public/javascript/app/initController.js
@@ -1,10 +1,19 @@
-app.controller('initController', function($scope, $location, authService){
+app.controller('initController', function($scope, $location, githubService, authService){
 
-  if(!authService.isAuth()){
+  if (!authService.isAuth()) {
     $location.path('/auth');
   }
 
-  $scope.goRepo = function (){
-    $location.path('/' + $scope.repo);
+  $scope.goRepo = function() {
+    githubService
+        .getReleases.apply(githubService, $scope.repo.split('/'))
+        .then(
+        function(){
+          $location.path('/' + $scope.repo);
+        },
+        function(){
+          alert('User or repository not found!');
+        }
+    );
   };
 });
--- public/javascript/app/mainController.js
+++ public/javascript/app/mainController.js
@@ -4,12 +4,17 @@ app.controller('mainController', function($scope, $location, $routeParams, githu
     if(!token){
       $location.path('/auth');
     }else{
-      $scope.gitProjectTitle = $routeParams.repo;
-      $scope.gitProjectAuthor = $routeParams.username;
-      githubService.getReleases($routeParams.username, $routeParams.repo)
+      githubService
+          .getReleases($routeParams.username, $routeParams.repo)
           .then(
           function(releases){
             $scope.changelog = releases;
+            $scope.gitProjectTitle = $routeParams.repo;
+            $scope.gitProjectAuthor = $routeParams.username;
+          },
+          function(response){
+            alert('User or repository not found!');
+            $route.reload();
           }
       );
 
